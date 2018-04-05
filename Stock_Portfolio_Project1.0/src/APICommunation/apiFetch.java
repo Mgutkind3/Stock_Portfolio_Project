8befb9850c92599923451da96052585603197456
Michael Gutkind <mikegutkind@gmail.com>
2018-04-05 13:46:11 -0400

    push for apiFetch and stock parse

---
+++ Stock_Portfolio_Project1.0/src/APICommunation/apiFetch.java
@@ -0,0 +1,36 @@
+package APICommunation;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class apiFetch {
+	
+	
+	//retrieve json from a url string and return it to the main
+			public static String getJson(String url) throws Exception{
+			try {
+			      URL request = new URL(url);
+			      URLConnection connection = request.openConnection();
+			      
+			      //timeout if connection is stuck
+			      connection.setConnectTimeout(1000);
+			      connection.setReadTimeout(1000);
+
+			      InputStreamReader inputStream = new InputStreamReader(connection.getInputStream(), "UTF-8");
+			      BufferedReader bufferedReader = new BufferedReader(inputStream);
+			      StringBuilder responseBuilder = new StringBuilder();
+
+			      String line;
+			      while ((line = bufferedReader.readLine()) != null) {
+			        responseBuilder.append(line);
+			      }
+			      bufferedReader.close();
+			      return responseBuilder.toString();
+			    } catch (IOException e) {
+			      throw new Exception("failure sending request", e);
+			    }
+			}
+}
