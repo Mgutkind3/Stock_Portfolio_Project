a52331129f2a4e16f23bfc221e5fa84b7e06130e
Henning Kjoeita <37564081+Hennns@users.noreply.github.com>
2018-04-10 14:18:56 -0400

    Mikes branch (#4)
    * Stock Search Panel is fully functional and working succussfully
    
    * Integrated Hennings dataPanel Code into the full project.
    
    * integrated graph with the current stock search page
    
    * Working on implimenting news data into stock search page.
    
    * integrated all of hennings code updates besides apiFetchFunction. Did
    not know the login.
    
    * integrated password and login screen.
    
    * add chartPanel to project
    
    * Add files via upload

--- Stock_Portfolio_Project1.0/src/APICommunation/apiFetchFunction.java
+++ Stock_Portfolio_Project1.0/src/APICommunation/apiFetchFunction.java
@@ -1,26 +1,21 @@
 package APICommunation;
-
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.URL;
-import java.net.URLConnection;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-import java.awt.BorderLayout;
+import java.util.Arrays;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 import javax.swing.border.TitledBorder;
 import javax.swing.*;
 
 //add pasword protection?
 //java card layout
-//look into jfree chart library
 public class apiFetchFunction {
-	
+		
 	//objects for referencing java ui panels
 	static summaryPanel sumPanel = new summaryPanel();
 	static yourStocksPanel yourSPanel = new yourStocksPanel();
@@ -30,7 +25,23 @@ public class apiFetchFunction {
 	static apiFetch jsonFetch = new apiFetch();
 	
 	public static void main(String[] args) throws Exception {
-		// TODO Auto-generated method stub
+		
+		//create menu frame
+		JFrame menu = new JFrame("Stock Ticker Menu");
+		menu.setSize(500, 250);
+		menu.setLayout(new FlowLayout());
+		menu.setLocationRelativeTo(null);
+		menu.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		menu.setMinimumSize(new Dimension(400, 200));
+		
+		JLabel passwordLabel = new JLabel("Enter password");
+		JPasswordField passwordField = new JPasswordField(20); 
+		passwordField.setToolTipText("Please enter password");
+		menu.add(passwordLabel);
+		menu.add(passwordField);
+		JButton enter = new JButton("Login");  
+		menu.add(enter);
+		menu.setVisible(true);
 	        
 				//TODO test url for AAPL call
 				URL testURL = new URL("https://api.iextrading.com/1.0/stock/market/batch?symbols=aapl&types=quote");
@@ -42,7 +53,7 @@ public class apiFetchFunction {
 				
 				//initialize frame for UI
 				JFrame frame = new JFrame("Stock Ticker");
-				frame.setSize(600, 500);
+				frame.setSize(750, 650);
 				JTabbedPane tp = new JTabbedPane();
 				
 				//create scroll panes for every page
@@ -64,7 +75,38 @@ public class apiFetchFunction {
 				frame.setLocationRelativeTo(null);
 				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 				frame.setMinimumSize(new Dimension(400, 200));
-				frame.setVisible(true);
+				
+				//the enter button on login screen
+				enter.addActionListener(new ActionListener() {  
+					    public void actionPerformed(ActionEvent e) {  
+					    	if (Arrays.equals(passwordField.getPassword(), new char[]{'p','a','s','s','w','o','r','d'})){   
+					    	   passwordField.setText("");
+					    	   menu.setVisible(false);
+					       	   frame.setVisible(true);
+					       }
+					    }  
+					    }); 
+					
+				//hit enter to log in
+				passwordField.addKeyListener(new KeyListener(){
+			            @Override
+			            public void keyTyped(KeyEvent e) {
+			                if(e.getKeyChar()==KeyEvent.VK_ENTER){
+			                    enter.doClick();
+			                }
+			            }
+
+			            @Override
+			            public void keyPressed(KeyEvent e) {
+			                //Do Nothing
+			            }
+
+			            @Override
+			            public void keyReleased(KeyEvent e) {
+			                //Do Nothing
+			            }
+
+			   	 });
 			}//end of main
 
 				
@@ -78,8 +120,9 @@ public class apiFetchFunction {
 
 		return title;
 	}
-
 	
+
+	  
 
 			
 }
